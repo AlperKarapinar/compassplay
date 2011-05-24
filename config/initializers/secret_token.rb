b30654f9981b46edc2e35d83d8097cc637b47d4a
Alper Karapınar <alper.karapinar@yh.com.tr>
2011-05-24 17:25:24 +0300

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Compassplay::Application.config.secret_token = '3cd9893a544045f17853cc71f2127f34da696a3efe680c3b9ae7716bad720df0efa1fab941a34d432c90406aa741aa48250b46a8d4ff4324b23440fc224bd3ce'
